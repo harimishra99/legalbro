7578396b265ec6bcb30debd7eb265d0351409c6d
Hari Shankar Mishra <harimishra.professional@gmail.com>
2026-05-10 17:49:14 +0530

    Commit 6

---
+++ backend/api/index.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+# Add backend root to path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+from django.core.wsgi import get_wsgi_application
+app = get_wsgi_application()
