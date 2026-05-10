d77e18d9743b9a47d024577c519fa5e9ddf0af32
Hari Shankar Mishra <harimishra.professional@gmail.com>
2026-05-10 23:45:36 +0530

    force static serving on Vercel

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("documents.urls")),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
